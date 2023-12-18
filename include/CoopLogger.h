6b6a110a524081911b74cae0122341b64263633e
Clifford B. Brown <cbrown350@users.noreply.github.com>
2023-12-18 04:10:38 -0700

    Update to include debug levels

--- include/CoopLogger.h
+++ include/CoopLogger.h
@@ -12,10 +12,11 @@
 class CoopLogger {
     protected:
         CoopLogger() = default;
-        static Print *defaultPrintStream;
+        inline static Print *defaultPrintStream = &Serial;
+        inline static std::mutex logMutex = std::mutex();
 
     private:
-        static std::unique_ptr<std::vector<std::unique_ptr<CoopLogger>>> loggers;
+        inline static std::unique_ptr<std::vector<std::unique_ptr<CoopLogger>>> loggers = nullptr;
 
         virtual void v_logv(const char *msg) { /* do nothing if not enabled */ };
         virtual void v_logd(const char *msg) { /* do nothing if not enabled */ };
@@ -23,11 +24,14 @@ class CoopLogger {
         virtual void v_logw(const char *msg) { /* do nothing if not enabled */ };
         virtual void v_loge(const char *msg) { /* do nothing if not enabled */ };
 
+        static void dummy(...) {};
+
     public:
         virtual ~CoopLogger() = default;
         CoopLogger(const CoopLogger& obj) = delete; 
             
         static void setDefaultPrintStream(Print *_defaultPrintStream) {
+            // TODO: implement wrapper with mutex locking?
             CoopLogger::defaultPrintStream = _defaultPrintStream;
         }
         static Print *getDefaultPrintStream() {
@@ -45,91 +49,102 @@ class CoopLogger {
         }
 
         template<typename... Args>            
-        static void logv(const char* TAG, const char *msg, Args&&... args) { 
-#ifdef ENABLE_LOGGING
-            if(loggers == nullptr)
+        static void logv(const char* TAG, const char *msg, Args&&... args) {
+#if defined(ENABLE_LOGGING) && defined(COOP_LOG_LEVEL) && (COOP_LOG_LEVEL >= ARDUHAL_LOG_LEVEL_VERBOSE)
+            if(loggers == nullptr || loggers->empty())
                 return;
             const std::string tag_msg = std::string("[%-*.*s] ") + msg;
             auto size = std::snprintf(nullptr, 0, tag_msg.c_str(), LOG_TAG_MAX_LEN, LOG_TAG_MAX_LEN, TAG, std::forward<Args>(args)...);
             std::string parsed_msg(size + 1, '\0');
             std::sprintf(&parsed_msg[0], tag_msg.c_str(), LOG_TAG_MAX_LEN, LOG_TAG_MAX_LEN, TAG, std::forward<Args>(args)...);
+            std::scoped_lock l(logMutex);
             for(auto const &logger : *loggers)
                 logger->v_logv(parsed_msg.c_str());
 #else                
             (void)TAG;
             (void)msg;
-            (void)args;
+//            (void)args;
+            dummy(std::forward<Args>(args)...);
 #endif
         }
+
         template<typename... Args>            
-        static void logd(const char* TAG, const char *msg, Args&&... args) { 
-#ifdef ENABLE_LOGGING
-            if(loggers == nullptr)
+        static void logd(const char* TAG, const char *msg, Args&&... args) {
+#if defined(ENABLE_LOGGING) && defined(COOP_LOG_LEVEL) && COOP_LOG_LEVEL >= ARDUHAL_LOG_LEVEL_DEBUG
+            if(loggers == nullptr || loggers->empty())
                 return;
             const std::string tag_msg = std::string("[%-*.*s] ") + msg;
             auto size = std::snprintf(nullptr, 0, tag_msg.c_str(), LOG_TAG_MAX_LEN, LOG_TAG_MAX_LEN, TAG, std::forward<Args>(args)...);
             std::string parsed_msg(size + 1, '\0');
             std::sprintf(&parsed_msg[0], tag_msg.c_str(), LOG_TAG_MAX_LEN, LOG_TAG_MAX_LEN, TAG, std::forward<Args>(args)...);
+            std::scoped_lock l(logMutex);
             for(auto const &logger : *loggers)
                 logger->v_logd(parsed_msg.c_str());
 #else                
             (void)TAG;
             (void)msg;
-            (void)args;
+//            (void)args;
+            dummy(std::forward<Args>(args)...);
 #endif
         }
     
         template<typename... Args>            
-        static void logi(const char *TAG, const char *msg, Args&&... args) {   
-#ifdef ENABLE_LOGGING
-            if(loggers == nullptr)
+        static void logi(const char *TAG, const char *msg, Args&&... args) {
+#if defined(ENABLE_LOGGING) && defined(COOP_LOG_LEVEL) && COOP_LOG_LEVEL >= ARDUHAL_LOG_LEVEL_INFO
+            if(loggers == nullptr || loggers->empty())
                 return;
             const std::string tag_msg = std::string("[%-*.*s] ") + msg;
             auto size = std::snprintf(nullptr, 0, tag_msg.c_str(), LOG_TAG_MAX_LEN, LOG_TAG_MAX_LEN, TAG, std::forward<Args>(args)...);
             std::string parsed_msg(size + 1, '\0');
             std::sprintf(&parsed_msg[0], tag_msg.c_str(), LOG_TAG_MAX_LEN, LOG_TAG_MAX_LEN, TAG, std::forward<Args>(args)...);
+            std::scoped_lock l(logMutex);
             for(auto const &logger : *loggers)
                 logger->v_logi(parsed_msg.c_str());
 #else                
             (void)TAG;
             (void)msg;
-            (void)args;
+//            (void)args;
+            dummy(std::forward<Args>(args)...);
 #endif
         }
 
-
         template<typename... Args>            
-        static void logw(const char* TAG, const char *msg, Args&&... args) { 
-#ifdef ENABLE_LOGGING
-            if(loggers == nullptr)
+        static void logw(const char* TAG, const char *msg, Args&&... args) {
+#if defined(ENABLE_LOGGING) && defined(COOP_LOG_LEVEL) && COOP_LOG_LEVEL >= ARDUHAL_LOG_LEVEL_WARN
+            if(loggers == nullptr || loggers->empty())
                 return;
             const std::string tag_msg = std::string("[%-*.*s] ") + msg;
             auto size = std::snprintf(nullptr, 0, tag_msg.c_str(), LOG_TAG_MAX_LEN, LOG_TAG_MAX_LEN, TAG, std::forward<Args>(args)...);
             std::string parsed_msg(size + 1, '\0');
             std::sprintf(&parsed_msg[0], tag_msg.c_str(), LOG_TAG_MAX_LEN, LOG_TAG_MAX_LEN, TAG, std::forward<Args>(args)...);
+            std::scoped_lock l(logMutex);
             for(auto const &logger : *loggers)
                 logger->v_logw(parsed_msg.c_str());
 #else                
             (void)TAG;
             (void)msg;
-            (void)args;
+//            (void)args;
+            dummy(std::forward<Args>(args)...);
 #endif
         }
+
         template<typename... Args>            
-        static void loge(const char* TAG, const char *msg, Args&&... args) { 
-#ifdef ENABLE_LOGGING
-            if(loggers == nullptr)
+        static void loge(const char* TAG, const char *msg, Args&&... args) {
+#if defined(ENABLE_LOGGING) && defined(COOP_LOG_LEVEL) && COOP_LOG_LEVEL >= ARDUHAL_LOG_LEVEL_ERROR
+            if(loggers == nullptr || loggers->empty())
                 return;
             const std::string tag_msg = std::string("[%-*.*s] ") + msg;
             auto size = std::snprintf(nullptr, 0, tag_msg.c_str(), LOG_TAG_MAX_LEN, LOG_TAG_MAX_LEN, TAG, std::forward<Args>(args)...);
             std::string parsed_msg(size + 1, '\0');
             std::sprintf(&parsed_msg[0], tag_msg.c_str(), LOG_TAG_MAX_LEN, LOG_TAG_MAX_LEN, TAG, std::forward<Args>(args)...);
+            std::scoped_lock l(logMutex);
             for(auto const &logger : *loggers)
                 logger->v_loge(parsed_msg.c_str());
 #else                
             (void)TAG;
             (void)msg;
-            (void)args;
+//            (void)args;
+            dummy(std::forward<Args>(args)...);
 #endif
         }       
 };
